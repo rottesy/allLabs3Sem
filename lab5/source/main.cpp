a9f344a50a2622e58066e4aac88052a7e398269b
Rottesy1 <egorkozel999@gmail.com>
2025-09-24 16:00:43 +0300

    added cmake and findMinEl function

---
+++ lab5/source/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+template<typename T>
+T findMin(const T* begin,const T* end)
+{
+    if(begin == end)
+    {
+        std::cout << "Array cannot be empty" << "\n";
+    }
+
+    T minVal = *begin;
+    for (const T* it = begin + 1;it != end;it++)
+    {
+        if (*it > minVal)
+        {
+            minVal = *it;
+        }
+    }
+    return minVal;
+}
