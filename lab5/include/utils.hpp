0972e2b3ff41283e596bb2f645cbd829fe8e274b
rottesy <egorkozel777@gmail.com>
2025-09-27 21:25:55 +0300

    added utils.hpp

---
+++ lab5/include/utils.hpp
@@ -0,0 +1,19 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <iostream>
+
+template<typename T>
+T getInput(const char* msg);
+
+template<typename T>
+T findMin(const T* begin,const T* end);
+
+template<typename T>
+T inputArray(T* arr,int size);
+
+template<typename T>
+T printArray(const T* arr,int size);
+
+
+#endif
