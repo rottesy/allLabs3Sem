15611fb596baedc720bc50b78d13a30ff1c8c692
rottesy <egorkozel777@gmail.com>
2025-09-27 21:33:29 +0300

    added utils.cpp and edited utils.hpp

--- lab5/include/utils.hpp
+++ lab5/include/utils.hpp
@@ -3,17 +3,12 @@
 
 #include <iostream>
 
-template<typename T>
-T getInput(const char* msg);
+template <typename T> T getInput(const char *msg);
 
-template<typename T>
-T findMin(const T* begin,const T* end);
+template <typename T> T findMin(const T *begin, const T *end);
 
-template<typename T>
-T inputArray(T* arr,int size);
+template <typename T> T inputArray(T *arr, int size);
 
-template<typename T>
-T printArray(const T* arr,int size);
-
+template <typename T> T printArray(const T *arr, int size);
 
 #endif
